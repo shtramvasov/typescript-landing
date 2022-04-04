eeb942c1a239827264e636020756c014d2f7c84a
IrishAir <bls21@mail.ru>
2022-04-04 13:56:11 +0500

    mainpage header animations

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { motion } from 'framer-motion'
 import styles from '../HeaderMenu/headermenu.module.css'
 import Logo from '../../img/logo.svg'
 
@@ -15,7 +16,11 @@ export const HeaderMenu: React.FC<Props> = (props) => {
   let accent: string = props.accent?.color === 'blue' ? 'blue' : 'yellow';
   return (
     <div className='app-wrapper'>
-      <div className={styles.container}>
+      <motion.div 
+        className={styles.container}
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 1.3, duration: 1}}>
           <Link to="/">
             <img src={Logo} alt="Perandus" />
           </Link>
@@ -24,13 +29,20 @@ export const HeaderMenu: React.FC<Props> = (props) => {
           <Link className='menulink' to="/features">Features</Link>
           <Link className='menulink' to="/services">Services</Link>
         </nav>
-      </div>
-      <h1>
+      </motion.div>
+      <motion.h1
+        initial={{ opacity: 0, y: 80 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.3, 
+          duration: 1,
+          opacity: { duration: 1.8 }, 
+          y: { type: 'spring', duration: 1.4 } }}>
         {props.title}
         {accent && accent === 'blue' ? 
         <span className={styles.blue}> {props.accent?.text}</span> :
         <span className={styles.yellow}> {props.accent?.text}</span>}
-      </h1>
+      </motion.h1>
     </div>
   )
 }
--- src/components/MainpageHeader/MainpageHeader.tsx
+++ src/components/MainpageHeader/MainpageHeader.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HeaderMenu } from '../HeaderMenu/HeaderMenu'
+import { motion } from 'framer-motion'
 import styles from '../MainpageHeader/mainpageheader.module.css'
 import pyramid from '../../img/main-pyramid.png'
 import coins from '../../img/main-coins.png'
@@ -16,9 +17,33 @@ export const MainpageHeader: React.FC = () => {
         }}
       />
       <div className={styles.images}>
-        <img className={styles.coins} src={coins} alt="Coins" />
-        <img className={styles.pyramid} src={pyramid} alt="Pyramid" />
-        <img className={styles.chat} src={chat} alt="Chat" />
+        <motion.img 
+          className={styles.coins} src={coins} alt="Coins"
+          initial={{ opacity: 0, y: 550 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 1.3, 
+            duration: 1.5,
+            opacity: { duration: 1.5 },
+            y: { duration: 2 }}}/>
+        <motion.img 
+          className={styles.pyramid} src={pyramid} alt="Pyramid"
+          initial={{ opacity: 0, y: 120 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 1.6, 
+            duration: 0.8,
+            opacity: { duration: 1.3 },
+            y: { duration: 1 }}}/>
+        <motion.img 
+          className={styles.chat} src={chat} alt="Chat"
+          initial={{ opacity: 0, y: 350 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            delay: 1.3, 
+            duration: 1.3,
+            opacity: { duration: 1.5 },
+            y: { duration: 2 }}}/>
       </div>
     </div>
   )
