03f42553930fbd7d78b042dbb7f741281772e7d7
IrishAir <bls21@mail.ru>
2022-04-06 10:03:59 +0500

    app redirect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Routes ,Route } from 'react-router-dom';
+import { Routes , Route, Navigate } from 'react-router-dom';
 import { Mainpage } from './pages/Mainpage';
 import { Pricingpage } from './pages/Pricingpage';
 import { Featurespage } from './pages/Featurespage';
 import { Servicespage } from './pages/Servicespage';
 import { Orderpage } from './pages/Orderpage';
 import ScrollToTop from './utils/ScrollToTop';
+
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         <Route path="/features" element={<Featurespage />}/>
         <Route path="/services" element={<Servicespage />}/>
         <Route path="/order" element={<Orderpage />}/>
+        <Route 
+          path="*" 
+          element={<Navigate to="/" />}/>
       </Routes>
     </>
   );
